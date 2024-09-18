5d2b71f769f2b9eb07b8ce4af43a194191b1c2ce
Bruno Coelho <brunocoelho66@gmail.com>
2024-09-18 15:18:05 -0300

    test of fonts

--- emporio-das-pizzas/src/components/Header.jsx
+++ emporio-das-pizzas/src/components/Header.jsx
@@ -2,7 +2,9 @@ import '../css/Header.css'
 
 function Header() {
     return (
-        <div className="header">Header</div>
+        <div className="header">
+            <h1>Header</h1>
+        </div>
     )
 }
 
