047b68445a04f6cb1716b528ac403eb267803998
Bruno Coelho <brunocoelho66@gmail.com>
2024-09-19 11:44:13 -0300

    style cardapio

--- emporio-das-pizzas/src/components/Cardapio.jsx
+++ emporio-das-pizzas/src/components/Cardapio.jsx
@@ -1,3 +1,5 @@
+import '../css/Cardapio.css'
+
 function Cardapio() {
     return (
         <section id="menu" className="menu-section">
@@ -5,40 +7,212 @@ function Cardapio() {
 
             <h3>Pizzas Tradicionais</h3>
             <ul>
-                <li>Calabresa c/ cheddar (queijo, cebola, calabresa, cheddar)
+                <li>
+                    <div className="image-pizza calabresa">
+                    </div>
+                    <p>
+                        Calabresa com cheddar
+                        <br />
+                        (queijo, cebola, calabresa, cheddar)
+                    </p>
                 </li>
-                <li>Calabresa (queijo, cebola e calabresa)
+                <li>
+                    <div className="image-pizza calabresa">
+                    </div>
+                    <p>
+                        Calabresa
+                        <br />
+                        (queijo, cebola e calabresa)
+                    </p>
                 </li>
-                <li>Portuguesa (queijo, presunto, cebola, milho e ovo)
+                <li>
+                    <div className="image-pizza portuguesa">
+                    </div>
+                    <p>
+                        Portuguesa
+                        <br />
+                        (queijo, presunto, cebola, milho e ovo)
+                    </p>
                 </li>
-                <li>Frango com bacon (frango, queijo e bacon)
+                <li>
+                    <div className="image-pizza frango">
+                    </div>
+                    <p>
+                        Frango com bacon
+                        <br />
+                        (frango, queijo e bacon)
+                    </p>
                 </li>
-                <li>Frango com cheddar (frango, queijo e cheddar)
+                <li>
+                    <div className="image-pizza frangoQueijo">
+                    </div>
+                    <p>
+                        Frango com cheddar
+                        <br />
+                        (frango, queijo e cheddar)
+                    </p>
                 </li>
-                <li>Frango com catupiry (frango, queijo e catupiry)
+                <li>
+                    <div className="image-pizza frangoQueijo">
+                    </div>
+                    <p>
+                        Frango com catupiry
+                        <br />
+                        (frango, queijo e catupiry)
+                    </p>
                 </li>
-                <li>Frango (frango e queijo)
+                <li>
+                    <div className="image-pizza frangoQueijo">
+                    </div>
+                    <p>
+                        Frango
+                        <br />
+                        (frango e queijo)
+                    </p>
                 </li>
-                <li>2 Queijos (catupiry e mussarela)
+                <li>
+                    <div className="image-pizza queijo">
+                    </div>
+                    <p>
+                        2 Queijos
+                        <br />
+                        (catupiry e mussarela)
+                    </p>
                 </li>
-                <li>4 Queijos (catupiry, mussarela, cheddar e parmesão)
+                <li>
+                    <div className="image-pizza queijo">
+                    </div>
+                    <p>
+                        4 Queijos
+                        <br />
+                        (catupiry, mussarela, cheddar e parmesão)
+                    </p>
                 </li>
             </ul>
 
             <h3>Pizzas Doces</h3>
             <ul>
-                <li>Dois Amores (chocolate branco e preto)</li>
-                <li>MM (chocolate e MM)</li>
-                <li>Brigadeiro (chocolate e granulado)</li>
-                {/* Continue listando as pizzas doces... */}
+                <li>
+                    <div className="image-pizza doisAmores">
+                    </div>
+                    <p>
+                        Dois Amores
+                        <br />
+                        (chocolate branco e preto)
+                    </p>
+                </li>
+                <li>
+                    <div className="image-pizza MM">
+                    </div>
+                    <p>
+                        M&M
+                        <br />
+                        (chocolate e MM)
+                    </p>
+                </li>
+                <li>
+                    <div className="image-pizza brigadeiro">
+                    </div>
+                    <p>
+                        Brigadeiro
+                        <br />
+                        (chocolate e granulado)
+                    </p>
+                </li>
+                <li>
+                    <div className="image-pizza beijinho">
+                    </div>
+                    <p>
+                        Beijinho
+                        <br />
+                        (chocolate branco e coco)
+                    </p>
+                </li>
+                <li>
+                    <div className="image-pizza romeuEJulieta">
+                    </div>
+                    <p>
+                        Romeu e Julieta
+                        <br />
+                        (mussarela e goiabada)
+                    </p>
+                </li>
             </ul>
 
             <h3>Pizzas Especiais</h3>
             <ul>
-                <li>Camarão (camarão e queijo)</li>
-                <li>Atum (atum, mussarela e cebola)</li>
-                <li>Peru Defumado (mussarela, peito de peru e catupiry)</li>
-                {/* Continue listando as pizzas especiais... */}
+                <li>
+                    <div className="image-pizza camarao">
+                    </div>
+                    <p>
+                        Camarão
+                        <br />
+                        (mussarela e queijo)
+                    </p>
+                </li>
+                <li>
+                    <div className="image-pizza atum">
+                    </div>
+                    <p>
+                        Atum
+                        <br />
+                        (atum, mussarela e cebola)
+                    </p>
+                </li>
+                <li>
+                    <div className="image-pizza peru">
+                    </div>
+                    <p>
+                        Peru defumado
+                        <br />
+                        (mussarela, peito de peru e catupiry)
+                    </p>
+                </li>
+                <li>
+                    <div className="image-pizza lombinho">
+                    </div>
+                    <p>
+                        Lombinho
+                        <br />
+                        (mussarela, catupiry e lombo)
+                    </p>
+                </li>
+                <li>
+                    <div className="image-pizza charque">
+                    </div>
+                    <p>
+                        Charque com Catupiry
+                        <br />
+                        (charque, queijo e catupiry)
+                    </p>
+                </li>
+                <li>
+                    <div className="image-pizza carneDeSol">
+                    </div>
+                    <p>
+                        Carne de sol
+                        <br />
+                        (carne de sol, mussarela, catupiry e cebola)
+                    </p>
+                </li>
+                <li>
+                    <div className="image-pizza queijoDoReino">
+                    </div>
+                    <p>
+                        Queijo do reino
+                        <br />
+                        (mussarela e queijo do reino)
+                    </p>
+                </li>
+                <li>
+                    <div className="image-pizza bacon">
+                    </div>
+                    <p>
+                        Bacon
+                        <br />
+                        (mussarela, bacon e cebola)
+                    </p>
+                </li>
             </ul>
         </section>
     )
