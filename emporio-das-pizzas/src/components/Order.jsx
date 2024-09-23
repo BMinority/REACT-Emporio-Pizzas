5adad3e8bdaf5e87236ca9bdb2a8893bec9e2774
Bruno Coelho <brunocoelho66@gmail.com>
2024-09-23 11:26:50 -0300

    feat relogio aberto fechado

--- emporio-das-pizzas/src/components/Order.jsx
+++ emporio-das-pizzas/src/components/Order.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import '../css/Order.css';
+import Relogio from './Relogio';
 
 function Pedidos() {
-    // Estados para o Formulário de Endereço
+    const [aberto, setAberto] = useState(true);
+
     const [endereco, setEndereco] = useState({
         nome: '',
         cep: '',
@@ -19,7 +21,7 @@ function Pedidos() {
     const [sabores, setSabores] = useState([]);
     const [pedidos, setPedidos] = useState([]);
 
-    // Função para lidar com as mudanças do formulário de endereço
+    //mudanças do formulário de endereço
     const handleEnderecoChange = (e) => {
         setEndereco({
             ...endereco,
@@ -27,7 +29,6 @@ function Pedidos() {
         });
     };
 
-    // Função para salvar o endereço
     const salvarEndereco = () => {
         if (validarEndereco()) {
             setEnderecoSalvo(endereco);
@@ -46,24 +47,21 @@ function Pedidos() {
         }
     };
 
-    // Função para validar o formulário de endereço
     const validarEndereco = () => {
         return endereco.nome && endereco.rua && endereco.numero && endereco.bairro &&
             endereco.cidade && endereco.estado;
     };
 
-    // Função para editar o endereço
     const editarEndereco = () => {
         setEndereco(enderecoSalvo);
         setEnderecoSalvo(null);
     };
 
-    // Função para excluir o endereço
     const excluirEndereco = () => {
         setEnderecoSalvo(null);
     };
 
-    // Função para lidar com as seleções de sabores
+    //seleção de sabores
     const handleSaborChange = (e) => {
         const saborSelecionado = e.target.value;
         if (e.target.checked) {
@@ -75,7 +73,6 @@ function Pedidos() {
         }
     };
 
-    // Função para salvar um pedido
     const salvarPedido = () => {
         if (sabores.length > 0) {
             setPedidos([...pedidos, sabores]);
@@ -83,18 +80,15 @@ function Pedidos() {
         }
     };
 
-    // Função para limpar os sabores selecionados
     const limparSabores = () => {
         setSabores([]);
     };
 
-    // Função para excluir um pedido
     const excluirPedido = (index) => {
         const novosPedidos = pedidos.filter((_, i) => i !== index);
         setPedidos(novosPedidos);
     };
 
-    // Função para gerar a mensagem do WhatsApp
     const gerarMensagemWhatsApp = () => {
         if (!enderecoSalvo || pedidos.length === 0) return '';
 
@@ -110,120 +104,125 @@ function Pedidos() {
         return `${saudacao}${mensagemEndereco}\n${mensagemPedidos}\n\n${mensagemQuantidade}`;
     };
 
-    // URLs de WhatsApp com a mensagem personalizada
+    // URLs de WhatsApp
     const mensagem = encodeURIComponent(gerarMensagemWhatsApp());
     const whatsappUrl01 = `https://wa.me/5581988813531?text=${mensagem}`;
     const whatsappUrl02 = `https://wa.me/5581988241010?text=${mensagem}`;
 
     return (
         <section id="order" className="order-section">
-            <h3>Faça seu Pedido</h3>
+            <Relogio setAberto={setAberto} />
+            {aberto ? (
+                <>
+                    <h3>Faça seu Pedido</h3>
 
-            {/* Formulário de Endereço */}
-            {!enderecoSalvo && (
-                <div className="address-form">
-                    <label>
-                        Nome:
-                        <input type="text" name="nome" value={endereco.nome} onChange={handleEnderecoChange} required />
-                    </label>
-                    <label>
-                        CEP:
-                        <input type="text" name="cep" value={endereco.cep} onChange={handleEnderecoChange} />
-                    </label>
-                    <label>
-                        Rua:
-                        <input type="text" name="rua" value={endereco.rua} onChange={handleEnderecoChange} required />
-                    </label>
-                    <label>
-                        Número:
-                        <input type="text" name="numero" value={endereco.numero} onChange={handleEnderecoChange} required />
-                    </label>
-                    <label>
-                        Complemento:
-                        <input type="text" name="complemento" value={endereco.complemento} onChange={handleEnderecoChange} />
-                    </label>
-                    <label>
-                        Bairro:
-                        <input type="text" name="bairro" value={endereco.bairro} onChange={handleEnderecoChange} required />
-                    </label>
-                    <label>
-                        Cidade:
-                        <input type="text" name="cidade" value={endereco.cidade} onChange={handleEnderecoChange} required />
-                    </label>
-                    <label>
-                        Estado:
-                        <input type="text" name="estado" value={endereco.estado} onChange={handleEnderecoChange} required />
-                    </label>
-                    <div className='address-btn'>
-                        <button onClick={salvarEndereco}>Salvar Endereço</button>
-                        <button onClick={() => setEndereco({
-                            nome: '', cep: '', rua: '', numero: '', complemento: '',
-                            bairro: '', cidade: '', estado: ''
-                        })}>Limpar</button>
-                    </div>
-                </div>
-            )}
-
-            {/* Visualização do Endereço */}
-            {enderecoSalvo && (
-                <div className="address-view">
-                    <h4>Endereço de Entrega</h4>
-                    <p>
-                        Nome: {enderecoSalvo.nome}<br />
-                        {enderecoSalvo.cep}, {enderecoSalvo.rua}, {enderecoSalvo.numero}, {enderecoSalvo.complemento}<br />
-                        {enderecoSalvo.bairro}, {enderecoSalvo.cidade} - {enderecoSalvo.estado}
-                    </p>
-                    <button onClick={editarEndereco}>Editar Endereço</button>
-                    <button onClick={excluirEndereco}>Excluir Endereço</button>
-                </div>
-            )}
-
-            {/* Formulário de Sabores */}
-            <div className="flavors-form">
-                <h4>Escolha até 2 Sabores</h4>
-                <div className="flavors-options">
-                    {['Calabresa c/ cheddar', 'Calabresa', 'Portuguesa', 'Frango c/ bacon', 'Frango c/ cheddar',
-                        'Frango c/ catupiry', 'Frango', '2 Queijos', '4 Queijos', 'Dois amores', 'MM', 'Brigadeiro',
-                        'Beijinho', 'Romeu e Julieta', 'Camarão', 'Atum', 'Peru defumado', 'Lombinho', 'Charque c/ catupiry',
-                        'Carne de sol', 'Queijo do reino', 'Bacon'].map((sabor, index) => (
-                            <div key={index} className="flavor-option">
-                                <input type="checkbox" id={`sabor-${index}`} value={sabor}
-                                    checked={sabores.includes(sabor)}
-                                    onChange={handleSaborChange}
-                                    disabled={sabores.length >= 2 && !sabores.includes(sabor)} />
-                                <label htmlFor={`sabor-${index}`}>{sabor}</label>
+                    {!enderecoSalvo && (
+                        <div className="address-form">
+                            <label>
+                                Nome:
+                                <input type="text" name="nome" value={endereco.nome} onChange={handleEnderecoChange} required />
+                            </label>
+                            <label>
+                                CEP:
+                                <input type="text" name="cep" value={endereco.cep} onChange={handleEnderecoChange} />
+                            </label>
+                            <label>
+                                Rua:
+                                <input type="text" name="rua" value={endereco.rua} onChange={handleEnderecoChange} required />
+                            </label>
+                            <label>
+                                Número:
+                                <input type="text" name="numero" value={endereco.numero} onChange={handleEnderecoChange} required />
+                            </label>
+                            <label>
+                                Complemento:
+                                <input type="text" name="complemento" value={endereco.complemento} onChange={handleEnderecoChange} />
+                            </label>
+                            <label>
+                                Bairro:
+                                <input type="text" name="bairro" value={endereco.bairro} onChange={handleEnderecoChange} required />
+                            </label>
+                            <label>
+                                Cidade:
+                                <input type="text" name="cidade" value={endereco.cidade} onChange={handleEnderecoChange} required />
+                            </label>
+                            <label>
+                                Estado:
+                                <input type="text" name="estado" value={endereco.estado} onChange={handleEnderecoChange} required />
+                            </label>
+                            <div className='address-btn'>
+                                <button onClick={salvarEndereco}>Salvar Endereço</button>
+                                <button onClick={() => setEndereco({
+                                    nome: '', cep: '', rua: '', numero: '', complemento: '',
+                                    bairro: '', cidade: '', estado: ''
+                                })}>Limpar</button>
                             </div>
-                        ))}
-                </div>
-                <div>
-                    <button onClick={salvarPedido} disabled={sabores.length === 0}>Salvar Pedido</button>
-                    <button onClick={limparSabores}>Limpar Sabores</button>
-                </div>
-            </div>
+                        </div>
+                    )}
 
-            {/* Resumo dos Pedidos */}
-            <div className="order-summary">
-                <h4>Resumo dos Pedidos</h4>
-                <ul>
-                    {pedidos.map((pedido, index) => (
-                        <li key={index}>
-                            <div>{pedido.join(' e ')}</div>
-                            <button onClick={() => excluirPedido(index)}>Excluir</button>
-                        </li>
-                    ))}
-                </ul>
-                <p>Total de Pizzas: {pedidos.length}</p>
-            </div>
+                    {enderecoSalvo && (
+                        <div className="address-view">
+                            <h4>Endereço de Entrega</h4>
+                            <p>
+                                Nome: {enderecoSalvo.nome}<br />
+                                {enderecoSalvo.cep}, {enderecoSalvo.rua}, {enderecoSalvo.numero}, {enderecoSalvo.complemento}<br />
+                                {enderecoSalvo.bairro}, {enderecoSalvo.cidade} - {enderecoSalvo.estado}
+                            </p>
+                            <button onClick={editarEndereco}>Editar Endereço</button>
+                            <button onClick={excluirEndereco}>Excluir Endereço</button>
+                        </div>
+                    )}
 
-            {/* Botões do WhatsApp */}
-            <div className="whatsapp-buttons">
-                <a href={whatsappUrl01} target="_blank" rel="noopener noreferrer">
-                    Enviar para (81) 98881-3531
-                </a>
-                <a href={whatsappUrl02} target="_blank" rel="noopener noreferrer">
-                    Enviar para (81) 98824-1010
-                </a>
-            </div>
+                    <div className="flavors-form">
+                        <h4>Escolha até 2 Sabores</h4>
+                        <div className="flavors-options">
+                            {['Calabresa c/ cheddar', 'Calabresa', 'Portuguesa', 'Frango c/ bacon', 'Frango c/ cheddar',
+                                'Frango c/ catupiry', 'Frango', '2 Queijos', '4 Queijos', 'Dois amores', 'MM', 'Brigadeiro',
+                                'Beijinho', 'Romeu e Julieta', 'Camarão', 'Atum', 'Peru defumado', 'Lombinho', 'Charque c/ catupiry',
+                                'Carne de sol', 'Queijo do reino', 'Bacon'].map((sabor, index) => (
+                                    <div key={index} className="flavor-option">
+                                        <input type="checkbox" id={`sabor-${index}`} value={sabor}
+                                            checked={sabores.includes(sabor)}
+                                            onChange={handleSaborChange}
+                                            disabled={sabores.length >= 2 && !sabores.includes(sabor)} />
+                                        <label htmlFor={`sabor-${index}`}>{sabor}</label>
+                                    </div>
+                                ))}
+                        </div>
+                        <div>
+                            <button onClick={salvarPedido} disabled={sabores.length === 0}>Salvar Pedido</button>
+                            <button onClick={limparSabores}>Limpar Sabores</button>
+                        </div>
+                    </div>
+
+                    <div className="order-summary">
+                        <h4>Resumo dos Pedidos</h4>
+                        <ul>
+                            {pedidos.map((pedido, index) => (
+                                <li key={index}>
+                                    <div>{pedido.join(' e ')}</div>
+                                    <button onClick={() => excluirPedido(index)}>Excluir</button>
+                                </li>
+                            ))}
+                        </ul>
+                        <p>Total de Pizzas: {pedidos.length}</p>
+                    </div>
+
+                    <div className="whatsapp-buttons">
+                        <a href={whatsappUrl01} target="_blank" rel="noopener noreferrer">
+                            Enviar para (81) 98881-3531
+                        </a>
+                        <a href={whatsappUrl02} target="_blank" rel="noopener noreferrer">
+                            Enviar para (81) 98824-1010
+                        </a>
+                    </div>
+                </>
+            ) : (
+                <div className="fechado-mensagem">
+                    <h3 style={{ color: 'red' }}>A pizzaria está fechada. Por favor, volte das 17h às 22h30!</h3>
+                </div>
+            )}
+
         </section>
     );
 }
---
+++ emporio-das-pizzas/src/components/Relogio.jsx
@@ -0,0 +1,39 @@
+import '../css/Hour.css'
+import { useState, useEffect } from 'react';
+
+function Relogio({ setAberto }) {
+    const [horaAtual, setHoraAtual] = useState(new Date());
+    const [statusPizzaria, setStatusPizzaria] = useState('Fechado');
+
+    useEffect(() => {
+        // Atualiza a hora atual
+        const intervalId = setInterval(() => {
+            setHoraAtual(new Date());
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    useEffect(() => {
+        // status da pizzaria com base na hora atual
+        const hora = horaAtual.getHours();
+        const minutos = horaAtual.getMinutes();
+        const estaAberto = (hora > 16 && hora < 22) || (hora === 22 && minutos <= 30);
+
+        setStatusPizzaria(estaAberto ? 'Aberto' : 'Fechado');
+        setAberto(estaAberto);
+    }, [horaAtual, setAberto]);
+
+    const estiloStatus = {
+        color: statusPizzaria === 'Aberto' ? 'green' : 'red'
+    };
+
+    return (
+        <div className="relogio-container">
+            <h3>Hora Atual: {horaAtual.toLocaleTimeString()}</h3>
+            <h4 style={estiloStatus}>Status: {statusPizzaria}</h4>
+        </div>
+    );
+}
+
+export default Relogio;
